5bf481fda6a9cca90149be017e1c83e3cd6a180d
shiyutao1116 <55845414+shiyutao1116@users.noreply.github.com>
2022-02-06 22:05:36 +0800

    Add files via upload

---
+++ atcrowdfunding08-member-eureka/src/main/java/com/atguigu/crowd/CrowdMainClass.java
@@ -0,0 +1,18 @@
+package com.atguigu.crowd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author shiyutao
+ * @create 2022-01-23 13:04
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class CrowdMainClass {
+    public static void main(String[] args) {
+        SpringApplication.run(CrowdMainClass.class,args);
+    }
+
+}
